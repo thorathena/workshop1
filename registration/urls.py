18c02bd0cd05bfa451e4668e892a7104a64a3ad6
thorathena <thorathena28198@gmail.com>
2016-06-07 15:23:37 +0530

    chocolate

--- registration/urls.py
+++ registration/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
    url(r'^user/$', UserRegistrationView.as_view(), name='register_user'),
    url(r'^user/success/', TemplateView.as_view(template_name='success.html'),
        name='user_registration_success'),
-   url(r'^chocolate/add/',AddChocolateView.as_view(),name="add_chocolate")
+    url(r'^chocolate/add/',AddChocolateView.as_view(),name="add_chocolate"),
+    url( r'^chocolate/info/(?P<choco_id>\d+)/$', ChocolateDetailsView.as_view(), name="chocolate_info"),
+    url(r'^user/profile/edit/$', UserProfileUpdateView.as_view(), name='update_form'),
+    url(r'^user/profile/edit/success/$',
+        TemplateView.as_view(template_name='update_success.html'),
+        name='update_success'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -4,13 +4,21 @@ from braces.views import LoginRequiredMixin, AnonymousRequiredMixin
 from django.views.generic.edit import FormView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from registration.forms import *
-from django.views.generic import ListView
+from django.views.generic import *
 from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.http import Http404
 # Create your views here.
+class CurrentUserMixin(object):
+   model = User
+   def get_object(self, *args, **kwargs):
+      try: obj = super(CurrentUserMixin, self).get_object(*args, **kwargs)
+      except AttributeError: obj = self.request.user
+      return obj
+
 class Home(ListView):
    template_name="Index.html"
 
@@ -26,6 +34,8 @@ class UserRegistrationView(AnonymousRequiredMixin, FormView):
    def form_valid(self, form):
       form.save()
       return FormView.form_valid(self, form)
+
+
 class AddChocolateView(FormView):
    template_name = "add_chocolate.html"
    form_class = ChocolateAddForm
@@ -34,3 +44,25 @@ class AddChocolateView(FormView):
    def form_valid(self, form):
       form.save()
       return FormView.form_valid(self, form)
+
+class ChocolateDetailsView(DetailView):
+   template_name = "chocolate_detail.html"
+
+   def get_object(self, queryset=None):
+      choco_id = self.kwargs['choco_id']
+      obj = Chocolate.objects.get(id=choco_id)
+      if obj:
+         return obj
+      else:
+         raise Http404("No details Found.")
+
+
+class UserProfileUpdateView(LoginRequiredMixin, CurrentUserMixin, UpdateView):
+   model = User
+   fields = user_fields + user_extra_fields
+   template_name = 'update_form.html'
+   success_url = '/register/user/profile/edit/success'
+
+   def form_valid(self, form):
+      form.save()
+      return UpdateView.form_valid(self, form)
